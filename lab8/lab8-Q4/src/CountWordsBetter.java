f1473eec0291112e71f1c2d269edcd0ca707d32c
sumanla13a <lamasuman2@gmail.com>
2016-09-21 22:20:38 -0500

    adds better solution for counting words

--- lab8/lab8-Q4/src/CountWordsBetter.java
+++ lab8/lab8-Q4/src/CountWordsBetter.java
@@ -1,7 +1,18 @@
 import java.util.*;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 public class CountWordsBetter {
-	final Function<String, Predicate<String>> containsLetter = letter -> name -> name.contains("" + letter);
-	final Function<String, Predicate<String>> doesnotContainLetter = letter -> name -> !name.contains("" + letter);
+	final BiFunction<Character, Character, Predicate<String>> containsLetter = (letter1, letter2) -> name -> name.contains("" + letter1) && !name.contains("" + letter2);
+	public int countWordsBetter(List<String> words, char c, char d, int len) {
+		 int count = (int)words.stream().filter(containsLetter.apply(c, d)).count();
+		 return count;
+	}
+	public static void main(String[] args){
+		List<String> words = new ArrayList<String>();
+		words.add("Suman");
+		words.add("Lamaa");
+		words.add("sumy");
+		CountWordsBetter countWord = new CountWordsBetter();
+		System.out.println(countWord.countWordsBetter(words, 'm', 'y', 5));
+	}
 }
